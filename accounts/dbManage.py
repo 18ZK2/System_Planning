7d48b806dec40011cc2ab8f3e3ac9ef6e5c552a4
FtoyCimpOmnkHty <55353405+FtoyCimpOmnkHty@users.noreply.github.com>
2021-01-01 14:21:22 +0900

    名前検索機能追加

--- accounts/dbManage.py
+++ accounts/dbManage.py
@@ -1,16 +1,28 @@
+
+import sys
+sys.path.append('..')
+from myproject.settings import DATABASES
 import sqlite3
 from django.contrib import admin
 
-tableNames = [('django_migrations',), ('sqlite_sequence',), ('django_content_type',), ('auth_group_permissions',), ('auth_permission',), ('auth_group',), ('accounts_authuser',), ('accounts_authuser_groups',), ('accounts_authuser_user_permissions',), ('accounts_employeestate',), ('django_admin_log',), ('django_session',)]
-#select name from sqlite_master where type='table';
-
-def NameSearch(name):
-    print(name)
-    dbPath = "..\db.sqlite3"
+tableNames = [
+    ('django_migrations',), ('sqlite_sequence',), ('django_content_type',),
+   ('auth_group_permissions',), ('auth_permission',), ('auth_group',),
+  ('accounts_authuser',), ('accounts_authuser_groups',), ('accounts_authuser_user_permissions',),
+ ('accounts_employeestate',), ('django_admin_log',), ('django_session',)
+ ]
+def ConnectServer():
+    dbPath = DATABASES['default']['NAME']
     connection = sqlite3.connect(dbPath)
-    print(connection)
     cursor = connection.cursor()
-    print(cursor)
-    exeCode = 'select userID from '+tableNames[6][0]
+    return cursor
+
+#select name from sqlite_master where type='table';
+def NameSearch(name):
+    cursor = ConnectServer()
+    exeCode = 'select *from '+tableNames[6][0]+' where userID = '+"'"+name+"'"
     cursor.execute(exeCode)
-    return cursor.fetchall()
+    result = cursor.fetchall()
+    cursor.close()
+    return result
+
